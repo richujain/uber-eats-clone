756d6215962f26d0038eec178f0235fb6b4f7ff2
Richu <richujain1995@gmail.com>
2022-07-04 18:17:48 -0400

    Implemented Google Places to the search bar. Also added hooks to listen to search bar to update the list of restaurants.

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,10 +4,19 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
-export default function SearchBar() {
+//Places API goes here //--------------------------------
+const PLACES_API = "" 
+
+export default function SearchBar({ cityHandler }) {
   return (
     <View style={{ marginTop: 15, flexDirection: 'row' }}>
       <GooglePlacesAutocomplete 
+      query={{key: PLACES_API}}
+      onPress={(data, details = null) => {
+        console.log(data.description)
+        const city = data.description.split(',')[0]
+        cityHandler(city)
+      }}
       placeholder='Search'
       styles={{
         textInput: {
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,18 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
 import RestaurantItem, { localRestaurants } from '../components/RestaurantItem'
 
 const YELP_API_KEY = "Loco5voB7QlJT43paP6EHGT4zXvYqh-dy7SyZV_fQiql6Ld-AWh-1_coZLhNV3nDP2NR9C478I54JME9Qv_f6DYYbtIIh1BpRXpn2YBaBs-9lPSBZEZG7dkHY6DzYHYx"
-
+//console.log(process.env.REACT_APP_YELP_KEY)
 export default function Home() {
-    const [restaurantData, setRestaurantData] = React.useState(localRestaurants)
+    const [restaurantData, setRestaurantData] = useState(localRestaurants)
+    const [city, setCity] = useState("Toronto")
 
     const getRestaurantsFromYelp = () => {
-        const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=M1M2C3';
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
         const apiOptions = {
             headers: {
                 Authorization: `Bearer ${YELP_API_KEY}`,
@@ -19,29 +20,24 @@ export default function Home() {
         };
             return fetch(yelpUrl, apiOptions)
             .then((res) => res.json())
-            // .then(json => {
-            //     console.log(json);
-            // })
-            // .catch(err => console.error(err))
             .then((json) => setRestaurantData(json.businesses));
         };
     //useEffect functions take two parameters, 1. Callback function and 2. Dependency
     
     useEffect(() => {
         getRestaurantsFromYelp()
-        //console.log(getRestaurantsFromYelp())
-    }, [])
+    }, [city]) //This hook looks for a change in city variable, the function will be evoked right when it detects the change. 
 
 
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
         <View style={{ backgroundColor: "white", padding: 15 }}>
             <HeaderTabs />
-            <SearchBar />
+            <SearchBar cityHandler = {setCity} />
         </View>
         <ScrollView showsVerticalScrollIndicator={ false }>
             <Categories />
-            <RestaurantItem restaurantData={ restaurantData }/>
+            <RestaurantItem restaurantData={ restaurantData } />
         </ScrollView>
     </SafeAreaView>
   )
