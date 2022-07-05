5c9e51d6263163d49197c7a036928f814d7266e7
Richu <richujain1995@gmail.com>
2022-07-04 22:38:06 -0400

    Designed and implemented About restaurant for restaurant details.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
 
 export default function App() {
-  return <Home/>;
+  //return <Home/>;
+  return <RestaurantDetail/>;
 }
---
+++ components/RestaurantDetail/About.js
@@ -0,0 +1,39 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+const image = "https://images.unsplash.com/photo-1564759224907-65b945ff0e84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+const title = "Farmhouse Kitchen Thai Cuisine"
+const description = "Thai • Comfort Food • $$ • 🎫 • 4 ⭐ (2913+)"
+
+export default function About() {
+  return (
+    <View>
+      <RestaurantImage image={image}/>
+      <RestaurantTitle title={title} />
+      <RestaurantDescription description={description} />
+    </View>
+  )
+}
+
+const RestaurantImage = (props) => (
+    <Image source={{ uri: props.image }} style={{ width: '100%', height: 180 }}/>
+)
+
+const RestaurantTitle = (props) => (
+    <Text style={{ 
+        fontSize: 29,
+        fontWeight: '600',
+        marginTop: 10,
+        marginHorizontal: 15,
+
+     }}>{ props.title }</Text>
+)
+
+const RestaurantDescription = (props) => (
+    <Text style= {{
+        marginTop: 10,
+        marginHorizontal: 15,
+        fontWeight: '400',
+        fontSize: 15.5,
+    }}>{ props.description }</Text>
+)
